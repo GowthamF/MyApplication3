a7af5bfd64bb578796826035b1e453ed3432267b
GowthamF <40605124+GowthamF@users.noreply.github.com>
2018-07-16 21:53:00 +0530

    paths

--- app/src/main/java/lk/sliit/mad/myapplication/MainActivity.java
+++ app/src/main/java/lk/sliit/mad/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     ImageView imageView1,imageView2,drawingImageView;
     TextView textView1,textView2;
-    LinearLayout l1,l2,l3;
+    LinearLayout l1,l2,l3,l4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +36,15 @@ public class MainActivity extends AppCompatActivity {
         l1=findViewById(R.id.auditl1);
         l2=findViewById(R.id.post1);
         l3=findViewById(R.id.post2);
+        l4=findViewById(R.id.post3);
         int array1[]=new int[2];
         int array[]=new int[2];
         int array2[]=new int[2];
+        int array3[]=new int[2];
         l1.getLocationOnScreen(array);
         l2.getLocationOnScreen(array1);
         l3.getLocationOnScreen(array2);
+        l4.getLocationOnScreen(array3);
 
         float x=array[0];
         float y=array[1];
@@ -49,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         float y1=array1[1];
         float x2=array2[0];
         float y2=array2[1];
+        float x3=array3[0];
+        float y3=array3[1];
 
 
 
@@ -76,8 +81,15 @@ public class MainActivity extends AppCompatActivity {
 
        //p.moveTo(x,y);
       // p.lineTo(x1,y1);
+        //320dpi
         canvas.drawLine(x+25,y+35,x1+25,y1+35,paint);
         canvas.drawLine(x1+25,y1+35,x2+25,y2+35,paint);
+        canvas.drawLine(x2+25,y2+35,x3+25,y3+35,paint);
+        //420dpi
+      //  canvas.drawLine(x+10,y-70,x1+10,y1-70,paint);
+       // canvas.drawLine(x1+10,y1-70,x2+10,y2-70,paint);
+       // canvas.drawLine(x2+10,y2-70,x3+10,y3-70,paint);
+
       // canvas.drawPath(p,paint);
 
         textView1.setText("Start X "+x+"Start Y "+y+" "+dp);
